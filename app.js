dbedbe241aadbae6e26e0e3bd7449d08c02c50a2
Greg Carlin <gregcarlin1@gmail.com>
2015-09-19 20:09:35 -0400

    Setting up motions

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var controller = new Leap.Controller();
 
 var active = false;
 var y0 = 0;
+var pitch0 = 0;
+var roll0 = 0;
+var yaw0 = 0;
 
 var cap = function(n) {
   n = Math.min(n, 1);
@@ -25,20 +28,39 @@ controller.on('frame', function(frame) {
         // first open fist
         active = true;
         y0 = hand.palmPosition[1];
+        pitch0 = hand.pitch();
+        roll0 = hand.roll();
+        yaw0 = hand.yaw();
       }
 
       var y = hand.palmPosition[1] - y0;
       y /= 200;
       y = cap(y);
       if (y > 0) {
-        // TODO move drone up
+        console.log('up');
       } else {
-        // TODO move drone down
+        console.log('down');
       }
 
-      var pitch = cap(hand.pitch()); // forward (-1) and back (+1)
-      var roll = cap(hand.roll()); // left (+1) and right (-1)
-      var yaw = cap(hand.yaw()); // rotation left (-1) and right (+1)
+      var pitch = cap(hand.pitch() - pitch0); // forward (-1) and back (+1)
+      var roll = cap(hand.roll() - roll0); // left (+1) and right (-1)
+      var yaw = cap(hand.yaw() - yaw0); // rotation left (-1) and right (+1)
+
+      if (pitch > 0) {
+        console.log('back');
+      } else {
+        console.log('forward');
+      }
+      if (roll > 0) {
+        console.log('left');
+      } else {
+        console.log('right');
+      }
+      if (yaw > 0) {
+        console.log('turn right');
+      } else {
+        console.log('turn left');
+      }
     }
   }
 });
