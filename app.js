7379074345d4e0c6075a8ff070fc5f58e7a45e5f
Greg Carlin <gregcarlin1@gmail.com>
2015-09-19 20:17:13 -0400

    Drone controls

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var Leap = require('leapjs');
+var arDrone = require('ar-drone');
 
 var controller = new Leap.Controller();
+var drone = arDrone.createClient();
 
 var active = false;
 var y0 = 0;
@@ -13,6 +15,7 @@ var yaw0 = 0;
 var cap = function(n) {
   n = Math.min(n, 1);
   n = Math.max(n, -1);
+  n *= 0.2;
   return n;
 };
 
@@ -22,7 +25,7 @@ controller.on('frame', function(frame) {
 
     if (hand.grabStrength > 0.9) {
       active = false;
-      // TODO stop drone
+      drone.land();
     } else {
       if (!active) {
         // first open fist
@@ -31,6 +34,7 @@ controller.on('frame', function(frame) {
         pitch0 = hand.pitch();
         roll0 = hand.roll();
         yaw0 = hand.yaw();
+        drone.takeoff();
       }
 
       var y = hand.palmPosition[1] - y0;
@@ -38,8 +42,10 @@ controller.on('frame', function(frame) {
       y = cap(y);
       if (y > 0) {
         console.log('up');
+        drone.up(y);
       } else {
         console.log('down');
+        drone.down(-y);
       }
 
       var pitch = cap(hand.pitch() - pitch0); // forward (-1) and back (+1)
@@ -48,18 +54,24 @@ controller.on('frame', function(frame) {
 
       if (pitch > 0) {
         console.log('back');
+        drone.back(pitch);
       } else {
         console.log('forward');
+        drone.forward(-pitch);
       }
       if (roll > 0) {
         console.log('left');
+        drone.left(roll);
       } else {
         console.log('right');
+        drone.right(-roll);
       }
       if (yaw > 0) {
         console.log('turn right');
+        drone.clockwise(yaw);
       } else {
         console.log('turn left');
+        drone.clockwise(-yaw);
       }
     }
   }
